ffc87432795524c7d90e16884e4663a2db1ba44a
SK.DHAMODHARAN <90711765+SKDHAMODHARAN@users.noreply.github.com>
2023-05-20 18:28:24 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,16 @@
+# Class is the collection of objects and methods and it is bluprint or protype to create the new objects.....
+# the First Letter of class name should be Capital
+#syntax...
+ ''' class Classname :
+            Statement 1
+            Statement 2
+            .
+            .
+            .
+            Statement n
+ '''
+    
+# Example:
+      class Family:
+            pass
+   # which the meaning of pass is nothing
