165a748a6e44565bb71212e8ccc95ada0dbf3f29
SK.DHAMODHARAN <90711765+SKDHAMODHARAN@users.noreply.github.com>
2023-05-20 19:04:45 +0530

    Create Objects,Class,and Slef.py

---
+++ Objects,Class,and Slef.py
@@ -0,0 +1,10 @@
+class Peri:
+      def __init__(self,name,college):
+          self.name = name
+          self.college = college
+      def student(self):
+           print("this is {}".format(self.name)+" he is is studying in {}".format(self.college))
+dhamo = Peri("Dhamodharan", "Peri_IT")
+dhamo.student()
+   
+         
